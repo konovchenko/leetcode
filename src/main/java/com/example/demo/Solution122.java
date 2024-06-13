7d00b6104c6f6622aec6c8d3aa1ba1e4d3976a65
Anton Konovchenko <anton.konovchenko@gmail.com>
2024-06-13 17:07:48 +0100

    Solution122 added

---
+++ src/main/java/com/example/demo/Solution122.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+/**
+ * 122. Best Time to Buy and Sell Stock II
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ * <p>
+ * On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+ * However, you can buy it then immediately sell it on the same day.
+ * <p>
+ * Find and return the maximum profit you can achieve.
+ */
+public class Solution122 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
